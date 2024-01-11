dee464677c4ac6110ae08ffc53659d9858213314
itsabhinavm <itsabhinavm05@gmail.com>
2024-01-11 20:43:24 +0530

    Completed Sceince Section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,17 +20,22 @@ class _HomePageState extends State<HomePage> {
   final List<FlashCard> _flashCard = [
     FlashCard(
       question: Math.tex(
-        "Einstein's mass-energy equivalence?",
-        textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+        "Mass-energy\\ equation?",
+        textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 21),
       ),
-      answer: Math.tex('x^2 + y^2 = z^2'),
+      answer: Math.tex('E = mc^2',
+          textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 22)),
     ),
     FlashCard(
-        question: Math.tex("What is the formula of Ohmslaw ?"),
-        answer: Math.tex('\frac a b')),
+        question: Math.tex("Ideal\\ gas\\ law:",
+            textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 22)),
+        answer: Math.tex('pH = -\log_{10}[H^+]',
+            textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 22))),
     FlashCard(
-        question: Math.tex("What is the formula of charge ?"),
-        answer: Math.tex('\frac a b'))
+        question: Math.tex("Arrhenius\\ equation",
+            textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 22)),
+        answer: Math.tex("k = Ae^{-Ea/RT}",
+            textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 22)))
   ];
 
   int _currentIndex = 0;
@@ -40,80 +45,83 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: appBar(),
-        body: Column(
+        body: scienceSection());
+  }
+
+  Column scienceSection() {
+    return Column(
+      children: [
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 40,
+            Padding(padding: const EdgeInsets.only(left: 20)),
+            Text(
+              "Science : ",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 25),
             ),
+            SvgPicture.asset(
+              'assets/icons/science.svg',
+              height: 35,
+              width: 20,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Column(
+          children: [
+            // old gesture
+            SwipeDetector(
+                onSwipeRight: (Offset) {
+                  showNextCard();
+                },
+                onSwipeLeft: (offset) {
+                  showPreviousCard();
+                },
+                child: SizedBox(
+                  height: 250,
+                  width: 300,
+                  child: Center(
+                      child: Card(
+                          color: Color.fromARGB(255, 85, 196, 204),
+                          child: FlipCard(
+                              front: Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: FlashCardView(
+                                      text:
+                                          _flashCard[_currentIndex].question)),
+                              back: FlashCardView(
+                                  text: _flashCard[_currentIndex].answer)))),
+                )),
             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Padding(padding: const EdgeInsets.only(left: 20)),
-                Text(
-                  "Science : ",
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 25),
-                ),
-                SvgPicture.asset(
-                  'assets/icons/science.svg',
-                  height: 35,
-                  width: 20,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Column(
-              children: [
-                // old gesture
-                SwipeDetector(
-                    onSwipeRight: (Offset) {
-                      showNextCard();
-                    },
-                    onSwipeLeft: (offset) {
+                OutlinedButton.icon(
+                    onPressed: () {
                       showPreviousCard();
                     },
-                    child: SizedBox(
-                      height: 250,
-                      width: 300,
-                      child: Center(
-                          child: Card(
-                              color: Color.fromARGB(255, 85, 196, 204),
-                              child: FlipCard(
-                                  front: Padding(
-                                      padding: EdgeInsets.all(10),
-                                      child: FlashCardView(
-                                          text: _flashCard[_currentIndex]
-                                              .question)),
-                                  back: FlashCardView(
-                                      text:
-                                          _flashCard[_currentIndex].answer)))),
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    OutlinedButton.icon(
-                        onPressed: () {
-                          showPreviousCard();
-                        },
-                        icon: SvgPicture.asset('assets/icons/left.svg'),
-                        label: Text('Prev')),
-                    OutlinedButton.icon(
-                        onPressed: () {
-                          showNextCard();
-                        },
-                        icon: SvgPicture.asset('assets/icons/left.svg'),
-                        label: Text('Next'))
-                  ],
-                ),
+                    icon: SvgPicture.asset('assets/icons/left.svg'),
+                    label: Text('Prev')),
+                OutlinedButton.icon(
+                    onPressed: () {
+                      showNextCard();
+                    },
+                    icon: SvgPicture.asset('assets/icons/left.svg'),
+                    label: Text('Next'))
               ],
-            )
+            ),
           ],
-        ));
+        )
+      ],
+    );
   }
 
   void showNextCard() {
